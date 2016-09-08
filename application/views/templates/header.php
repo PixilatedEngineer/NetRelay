5473878cdcd4243722cc7df2cb7b27388caf4578
Grigory Morozov <negrusti@gmail.com>
2016-09-08 16:49:02 +0700

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,10 +2,10 @@
 <html>
 <head>
     <title>Relay Control</title>
-    <link rel="shortcut icon" href="/assets/images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo base_url();?>/assets/images/favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="/assets/css/style.css"/>
+    <link rel="stylesheet" href="<?php echo base_url();?>/assets/bootstrap/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/style.css"/>
 </head>
 <body>
     <div class="container">
