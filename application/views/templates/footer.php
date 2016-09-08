9ec9aab7ddd342f579e9faa296b7964deeeafe73
Grigory Morozov <negrusti@gmail.com>
2016-09-08 16:49:31 +0700

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,8 +1,8 @@
 </div>
 
-    <script src="/assets/js/jquery-1.12.2.min.js"></script>
-    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="/assets/js/script.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/jquery-1.12.2.min.js"></script>
+    <script src="<?php echo base_url();?>/assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/script.js"></script>
 
 </body>
 </html>
